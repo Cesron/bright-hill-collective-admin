29c13a53172665dca1eb3d7c5f75a4c5a12443c2
Adilson <cesronvasquez@gmail.com>
2025-07-08 23:09:20 -0600

    feat: enhance dashboard with stat cards and add new utility styles for glowing effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,54 @@
-import { sql } from "@/lib/sql"
+import { StatCard } from "@/components/stat-card"
 
 export default async function Home() {
-  const users = await sql`SELECT * FROM users`
-
-  const user_logins = await sql`SELECT * FROM user_logins`
-
   return (
-    <div>
-      <pre>
-        <code>{JSON.stringify(users, null, 2)}</code>
-      </pre>
+    <>
+      <header className="bg-header">
+        <div className="container mx-auto p-4">
+          <h1 className="text-primary-foreground text-xl font-bold">
+            Bright Hill Collective Admin
+          </h1>
+        </div>
+      </header>
 
-      <pre>
-        <code>{JSON.stringify(user_logins, null, 2)}</code>
-      </pre>
-    </div>
+      <main className="container mx-auto p-4">
+        <div>
+          <h2 className="text-primary-foreground text-lg font-bold">
+            Welcome, Admin
+          </h2>
+          <p>Start your day with a review of the latest updates.</p>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          <StatCard
+            type="students"
+            count={94}
+            label="Total students enrolled"
+            icon="/icons/students.svg"
+          />
+
+          <StatCard
+            type="completed"
+            count={42}
+            label="Total completed payments"
+            icon="/icons/completed.svg"
+          />
+
+          <StatCard
+            type="pending"
+            count={12}
+            label="Total pending payments"
+            icon="/icons/pending.svg"
+          />
+
+          <StatCard
+            type="overdue"
+            count={5}
+            label="Total overdue payments"
+            icon="/icons/overdue.svg"
+          />
+        </div>
+      </main>
+    </>
   )
 }
---
+++ src/components/stat-card.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image"
+
+import { cn } from "@/lib/cn"
+
+type StatCardProps = {
+  type: "students" | "completed" | "pending" | "overdue"
+  count: number
+  label: string
+  icon: string
+}
+
+export const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
+  return (
+    <div className="glass relative overflow-hidden rounded-xl p-6">
+      <div className="flex items-center gap-4">
+        <Image
+          src={icon}
+          height={32}
+          width={32}
+          alt="admin-icon"
+          className={cn("stat-icon size-8 w-fit", {
+            "yellow-glow": type === "students",
+            "green-glow": type === "completed",
+            "blue-glow": type === "pending",
+            "red-glow": type === "overdue",
+          })}
+        />
+        <h2 className="text-2xl font-bold text-white">{count}</h2>
+      </div>
+
+      <p className="mt-6">{label}</p>
+
+      <span
+        className={cn(
+          "pointer-events-none absolute bottom-0 left-6 z-0 block h-[60px] w-[120px] translate-y-1/2 rounded-t-[120px] rounded-b-none opacity-20 blur-3xl",
+          {
+            "bg-yellow-300": type === "students",
+            "bg-green-300": type === "completed",
+            "bg-blue-300": type === "pending",
+            "bg-red-300": type === "overdue",
+          },
+        )}
+      />
+    </div>
+  )
+}
